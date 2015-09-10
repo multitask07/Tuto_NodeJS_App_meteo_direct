42a29ca5769f13928cf6f4822d656a376052882d
david07 <david.malosse@voila.fr>
2015-09-10 11:37:56 +0200

    ajout du module http pour la requete GET

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+var http = require('http');
+
 var ville_zip = "07160,fr";
 
-var request = get("http://api.openweathermap.org/data/2.5/weather?zip="+ ville_zip, function(reponse){ //recupere la valeur du temps sur l'api de notre site et affichera celui de notre variable ville_zip
+var request = http.get("http://api.openweathermap.org/data/2.5/weather?zip="+ ville_zip, function(reponse){ //recupere la valeur du temps sur l'api de notre site et affichera celui de notre variable ville_zip
      console.dir(reponse);  // une fois qu'on appel cette requête elle sera toute executé
 });
