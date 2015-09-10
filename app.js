3f791a97acb94f12f821d2ee9f2e2b7d06c62490
david07 <david.malosse@voila.fr>
2015-09-10 12:13:14 +0200

    affiche sur notre console les données réunis valide précédés du préfixe body! de notre variable 'body'

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ var request = http.get("http://api.openweathermap.org/data/2.5/weather?zip="+ vi
 // on ne selectionne qu'une seul valeur de notre fichier json ici la ligne de la valeur 'statusCode' =200 si ok
      console.log(reponse.statusCode);
 
-     reponse.on('data', function (chunk) { // recuperation des données en portions, découpés (chunk)
-        console.log('BODY: ' + chunk); 
+     var body = ""; // nouvelle variable body qui va accueillir nos données
+// recuperation des données en portions, découpés (chunk)
+     reponse.on('data', function (chunk) { 
+        body += chunk; //incremente le préfixe body à nos données valides 
+     });
+// détection de la fin des instructions {fin du fichier} JSON ->end 
+     reponse.on('end', function () { 
+        console.log(body); // affiche sur notre console les données réunis valide précédés du préfixe body! de notre variable 'body'
      });  
 });     
