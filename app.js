43c65006e70ca291d519734cb0861e9bac7e933c
david07 <david.malosse@voila.fr>
2015-09-10 12:01:02 +0200

    ajout de la fonction reponse.on ('data' ) qui affiche sur notre console les données valide précédés du préfixe BODY

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ var ville_zip = "07160,fr";
 
 var request = http.get("http://api.openweathermap.org/data/2.5/weather?zip="+ ville_zip, function(reponse){ //recupere la valeur du temps sur l'api de notre site et affichera celui de notre variable ville_zip
 // on ne selectionne qu'une seul valeur de notre fichier json ici la ligne de la valeur 'statusCode' =200 si ok
-     console.log(reponse.statusCode);  
+     console.log(reponse.statusCode);
+
+     reponse.on('data', function (chunk) { // recuperation des données en portions, découpés (chunk)
+        console.log('BODY: ' + chunk); 
+     });  
 });     
