d4de7955b6fbad0246a02084fd93b07edd4e7129
david07 <david.malosse@voila.fr>
2015-09-10 11:57:00 +0200

    ajout d'une requete vers la valeur statusCode de notre fichier api pour savoir si il n'y a pas d'erreurs ->return 200

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ var http = require('http');
 var ville_zip = "07160,fr";
 
 var request = http.get("http://api.openweathermap.org/data/2.5/weather?zip="+ ville_zip, function(reponse){ //recupere la valeur du temps sur l'api de notre site et affichera celui de notre variable ville_zip
-     console.dir(reponse);  // une fois qu'on appel cette requête elle sera toute executé
-});
+// on ne selectionne qu'une seul valeur de notre fichier json ici la ligne de la valeur 'statusCode' =200 si ok
+     console.log(reponse.statusCode);  
+});     
