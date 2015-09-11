fe61a427682c9c9abbc854ac9b4bf0840ecf3ab5
hitman07 <david.malosse@voila.fr>
2015-09-11 03:13:55 +0200

    création et appel de notre fonction 'printMessage' et recupération des 3 valeurs dans notre objet Json 'data_meteorologique' de type clef:valeur

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var http = require('http');
 
 var ville_zip = "07160,fr";
 
+function printMessage(ville_zip, temperature, pression){ //fonction d'affichage des valeurs qui valeurs qui suivent
+	console.log("Au code postal "+ville_zip + ", la temperatureest de " +(temperature-273.15)+ "°C et la pression est de "+ pression +" bars");
+
+}
+
 var request = http.get("http://api.openweathermap.org/data/2.5/weather?zip="+ ville_zip, function(reponse){ //recupere la valeur du temps sur l'api de notre site et affichera celui de notre variable ville_zip
 // on ne selectionne qu'une seul valeur de notre fichier json ici la ligne de la valeur 'statusCode' =200 si ok
      console.log(reponse.statusCode);
@@ -14,6 +19,6 @@ var request = http.get("http://api.openweathermap.org/data/2.5/weather?zip="+ vi
 // détection de la fin des instructions {fin du fichier} JSON ->end 
      reponse.on('end', function () {
         var data_meteorologique = JSON.parse(body); //traduction de la variable body en fichier Json, & enregistrement de celle-ci dans une nouvelle variable type 'objet' ->data_meteorologique
-        console.log(data_meteorologique.main.temp); // affiche sur notre console la valeur donée par notre fichier Json ici la temperature en farenight {main.temp}
+        printMessage(ville_zip, data_meteorologique.main.temp, data_meteorologique.main.pressure);
      });
 });  //fin get
