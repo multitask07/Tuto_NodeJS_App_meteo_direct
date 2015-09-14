b621f0039b3cbe704ca14006314428aa3b3725f8
hitman07 <david.malosse@voila.fr>
2015-09-14 14:30:30 +0200

    recupération des codes et eventuelles erreurs du retour de notre API, et gestion de celles ci avec les conditions

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 var http = require('http');
 
-var ville_zip = "07160,fr";
 
-function printMessage(ville_zip, temperature, pression){ //fonction d'affichage des valeurs qui valeurs qui suivent
-	console.log("Au code postal "+ville_zip + ", la temperatureest de " +(temperature-273.15)+ "°C et la pression est de "+ pression +" bars");
+var ville_zip = "07160,fr"; //attibut une adresse à notre variable 'ville_zip'
 
+//******************crétion de nos fonctions /objets***************
+function printMessage(ville_zip, ville_select, temperature, pression){ //fonction d'affichage des valeurs qui valeurs qui suivent
+    console.log("Au code postal "+ville_zip + " à "+ville_select +", la temperature est de " +(temperature-273.15).toFixed(2)+ "°C et la pression est de "+ pression +" hpascals"); // toFixed ->arrondit à 2 chiffres après la virgule
 }
 
-var request = http.get("http://api.openweathermap.org/data/2.5/weather?zip="+ ville_zip, function(reponse){ //recupere la valeur du temps sur l'api de notre site et affichera celui de notre variable ville_zip
-// on ne selectionne qu'une seul valeur de notre fichier json ici la ligne de la valeur 'statusCode' =200 si ok
-     console.log(reponse.statusCode);
 
+//**************************début du GET *****************************
+var request = http.get("http://api.openweathermap.org/data/2.5/weather?zip="+ ville_zip, function(reponse){ //recupere la valeur du temps sur l'api de notre site et affichera celui indiqué par notre variable ville_zip qui determine l'adresse où sonder les valeurs
      var body = ""; // nouvelle variable body qui va accueillir nos données
-// recuperation des données en portions, découpés (chunk)
+
+     // recuperation des données en portions, découpés (chunk)
      reponse.on('data', function (chunk) { 
         body += chunk; //incremente le préfixe body à nos données valides 
      });
-// détection de la fin des instructions {fin du fichier} JSON ->end 
+
+     // détection de la fin des instructions {fin du fichier} JSON ->end 
      reponse.on('end', function () {
-        var data_meteorologique = JSON.parse(body); //traduction de la variable body en fichier Json, & enregistrement de celle-ci dans une nouvelle variable type 'objet' ->data_meteorologique
-        printMessage(ville_zip, data_meteorologique.main.temp, data_meteorologique.main.pressure);
+        //********gere la traduction du résultat recupéré en ligne et des erreurs de saisie, ou traitement eventuelles*********
+        if (reponse.statusCode === 200){ // on ne selectionne qu'une seul valeur de notre fichier json ici la ligne de la valeur 'statusCode' =200 si ok
+            try{ //faire, essayer d'executer la suite
+                var data_meteorologique = JSON.parse(body); //traduction de la variable body en fichier Json, & enregistrement de celle-ci dans une nouvelle variable type 'objet' ->data_meteorologique
+                printMessage(ville_zip, data_meteorologique.name, data_meteorologique.main.temp, data_meteorologique.main.pressure); //appel de notre fonction 'printMessage' et recupération des 4 valeurs (la premiere_que nous lui donnons =>ville_zip )& les 3 suivantes venant de notre objet Json 'data_meteorologique' de type clef:valeur; 
+            }catch(error){ //si le parse n'arrive pas a s-executer faire ce qui suit {affiche le mess d'erreur}
+                console.error("erreur_du code postale de la ville saisie");
+                }
+        }else console.log(reponse.statusCode)
      });
 });  //fin get
